b7dcd9dbf5ab601747b97110b27f4757d5fb27e2
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-06-04 10:37:21 +0100

    First commit

---
+++ src/Class.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Class {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        Integer a2 = a;
+        System.out.println(a2);
+    }
+}
